d173129294899cec43b9446dfacd8ca9f686ec76
argedd <ingargediaz@gmail.com>
2023-11-22 09:12:49 -0400

    Update serverConfig.js

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,15 +1,22 @@
 // config/serverConfig.js
 const express = require('express');
-const http = require('http');
+const https = require('https');
 const socketIo = require('socket.io');
 const cors = require('cors');
 const app = express();
 const socketManager = require('./socketManager'); // Ajusta la ruta según la ubicación real de socketManager.js
 
-const server = http.createServer(app);
+
+const credentials = {
+  key: fs.readFileSync('ruta/a/tu/clave-privada.pem', 'utf8'),
+  cert: fs.readFileSync('ruta/a/tu/certificado.pem', 'utf8'),
+  ca: fs.readFileSync('ruta/a/tu/chain.pem', 'utf8')
+};
+// const server = http.createServer(app);
+const server = https.createServer(credentials, app);
 
 const corsOptions = {
-  origin: ['*', 'https://localhost:4200'],
+  origin: ['*', 'http://localhost:4200'],
   methods: ['GET', 'POST', 'PATCH', 'PUT']
 };
 app.use(cors(corsOptions));
@@ -17,12 +24,12 @@ app.use(express.json());
 
 const io = socketIo(server, {
   cors: {
-    origin: ['*', 'https://localhost:4200'],
+    origin: ['*', 'http://localhost:4200'],
     methods: ['GET', 'POST', 'PATCH', 'PUT']
   }
 });
 app.get('/check',(req,res)=>{
-  res.status(200).json({message:'server is runnig',    sockets: Object.keys(io.sockets.connected).length,
+  res.status(200).json({message:'server is runnig',
 })
 })
 socketManager.initializeSocket(io); // Pasa la instancia de io y tickets al inicializar los sockets
