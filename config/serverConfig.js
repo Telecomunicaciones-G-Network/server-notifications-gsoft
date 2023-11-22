60694008d64b163cb40f68ffda80180883bda1ac
argedd <ingargediaz@gmail.com>
2023-11-22 11:27:26 -0400

    Update serverConfig.js

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,18 +1,19 @@
 // config/serverConfig.js
 const express = require('express');
 const https = require('https');
+const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
 const app = express();
 const socketManager = require('./socketManager'); // Ajusta la ruta según la ubicación real de socketManager.js
 const fs = require('fs');
 
-const credentials = {
-  key: fs.readFileSync('/etc/letsencrypt/live/tkwskt.gsoft.app/privkey.pem', 'utf8'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/tkwskt.gsoft.app/fullchain.pem', 'utf8')
-};
-// const server = http.createServer(app);
-const server = https.createServer(credentials, app);
+// const credentials = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/tkwskt.gsoft.app/privkey.pem', 'utf8'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/tkwskt.gsoft.app/fullchain.pem', 'utf8')
+// };
+const server = http.createServer(app);
+// const server = https.createServer(credentials, app);
 
 const corsOptions = {
   origin: ['*', 'http://localhost:4200'],
